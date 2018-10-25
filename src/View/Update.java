7a101318448ca3a2f5ed385402ecb6114f213584
moravitan <moravit@post.bgo.ac.il>
2018-10-25 14:43:59 +0300

    update

--- src/View/Update.java
+++ src/View/Update.java
@@ -12,6 +12,11 @@ public class Update implements Observer {
     private Stage stage;
 
 
+    /**
+     *
+     * @param controller
+     * @param stage
+     */
     void setController(Controller controller, Stage stage) {
         this.controller = controller;
         this.stage = stage;
