330a28fd42ca2d03ca5619602ffe50cbf65ebff1
Inbaroth <inbaroth1993@gmail.com>
2018-10-25 13:47:28 +0300

    test

--- src/View/test.java
+++ src/View/test.java
@@ -1,17 +1,12 @@
 package View;
 
 import Model.Model;
-import javafx.scene.input.DataFormat;
-
-import javax.xml.crypto.Data;
-import java.util.Date;
 
 public class test {
 
     public static void main(String[] args) {
 
-//        Model model = new Model();
-//        model.insert("mor","mor","mor","mor","mor","mor");
-//        model.read("mor");
+        Model model = new Model();
+        model.insert("mor","mor","mor","mor","mor","mor");
     }
 }
